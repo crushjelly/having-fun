b5ea722b2abc76b1f6a2551d6480205b50be390c
crushjelly <laurynasz94@gmail.com>
2022-09-04 21:24:01 +0300

    moving files

---
+++ FileValidatorProblem/src/Playground/Program.cs
@@ -0,0 +1,27 @@
+﻿using FilesValidatorProblem;
+using FilesValidatorProblem.Models;
+
+var files = GetFakeConfigFiles();
+
+try
+{
+    var usbFile = DeviceConfigFileHandler.Handle<UsbDeviceConfigFile>(files);
+    Console.WriteLine(usbFile?.Name);
+}
+catch (Exception e) { Console.WriteLine(e.Message); }
+
+try
+{
+    var obdDeviceConfigFile = DeviceConfigFileHandler.Handle<ObdDeviceConfigFile>(files);
+    Console.WriteLine($"{obdDeviceConfigFile?.Name} Version: {obdDeviceConfigFile?.Version}");
+}
+catch (Exception e) { Console.WriteLine(e.Message); }
+
+static List<BaseDeviceConfigFile> GetFakeConfigFiles()
+{
+    return new List<BaseDeviceConfigFile>()
+    {
+        new UsbDeviceConfigFile("UsbSettings", "1.0.0.0", ".json"),
+        new ObdDeviceConfigFile("ObdSettings", "0.0.0.1", ".config")
+    };
+}
